6eac1928242507db9f36bfa94ebe082ad4fe9e50
skimike02 <51763147+skimike02@users.noreply.github.com>
2023-07-14 22:10:18 -0700

    added request headers

--- inflation_treemap.py
+++ inflation_treemap.py
@@ -9,7 +9,21 @@ fileloc=config.fileloc
 
 #%% Get Data
 url='https://www.bls.gov/news.release/cpi.t02.htm'
-html=requests.get(url).text
+headers = {
+    'Sec-Ch-Ua': '"Not A(Brand";v="24", "Chromium";v="110"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.178 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.9'
+}
+html=requests.get(url,headers=headers).text
 soup = BeautifulSoup(html)
 output_file="inflation.html"
 
